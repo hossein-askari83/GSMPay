65ff84939ab5ed98e5ef3d8f095dbf58149ce29a
hossein-asakri <hosseinaskari.bamooz@gmail.com>
2025-08-02 15:45:39 +0330

    #5 Implement DD structure

--- app/Domains/Post/Models/Post.php
+++ app/Domains/Post/Models/Post.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Domains\Post\Models;
 
+use App\Domains\User\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
--- app/Domains/User/Models/User.php
+++ app/Domains/User/Models/User.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace App\Domains\User\Models;
 
+use App\Domains\Post\Models\Post;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -26,5 +28,21 @@ class User extends Authenticatable
     public function posts(): HasMany
     {
         return $this->hasMany(related: Post::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the JWT token.
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return an array with custom claims to be added to the JWT token.
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
     }
 }
